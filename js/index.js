0460fc3333232f5723ecb153b6d7da400cc07058
coding655 <codingmaster655@gmail.com>
2024-03-30 15:28:00 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const loadButton = () => {
+    const buttonField = document.getElementById('btn-clicked').addEventListener('click', function(){
+
+        window.location.href='Ans.html';
+
+    });
+  
+}
+// loadButton();
