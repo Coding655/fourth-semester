cd20b19782aa35cbac4587b262c26d223ba51a5d
coding655 <codingmaster655@gmail.com>
2024-03-30 16:53:20 +0600

    second commit

---
+++ js/login.js
@@ -0,0 +1,13 @@
+document.getElementById('btn-clicked').addEventListener('click', function(){
+    const emailField = document.getElementById('inputEmail');
+    const email = emailField.value;
+    const passField = document.getElementById('inputPass');
+    const pass = passField.value;
+    if(email === 'imransirajee@gmail.com' && pass === 'password'){
+        window.location.href='index.html'
+    }
+    else{
+        alert('WRONG PASSWORD');
+    }
+
+})
