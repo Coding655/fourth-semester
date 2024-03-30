eb6429a959cb8e090dc3c1c8e69eb4341371ecd1
coding655 <codingmaster655@gmail.com>
2024-03-30 15:52:14 +0600

    second commit

--- js/index.js
+++ js/index.js
@@ -1,9 +1,28 @@
-const loadButton = () => {
-    const buttonField = document.getElementById('btn-clicked').addEventListener('click', function(){
+let slideIndex = 1;
+showSlides(slideIndex);
 
-        window.location.href='Ans.html';
-
-    });
-  
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
-// loadButton();
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
